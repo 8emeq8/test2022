faa4edf9f403592b56a168d5e012c2226d37e011
8emeq8 <85879063+8emeq8@users.noreply.github.com>
2022-02-21 08:07:39 +0300

    commit it

--- __manifest__.py
+++ __manifest__.py
@@ -1,30 +1,24 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
-    'name': 'Account Type Menu',
-    'version': '14.0.1.0',
-    'category': 'accounting',
-    'summary': 'Account Type is a parent account being used in Odoo Chart of Account to defined child accounts',    # Author
-    'author': 'Mediod Consulting Pvt. Ltd.',
-    'website': 'http://www.mediodconsulting.com/',
-    'maintainer': 'Mediod Consulting Pvt. Ltd.',
-    'description': """This module will add menu under account configuration menu to add a new menu for account.account.type
-
-Account Type is a parent account being used in Chart of Account to defined child accounts.
-
-menu to add or replace or update account type in chart of account.
-Odoo Odoo Accounts""",
-    'sequence': -100,
-    'depends': ['account'],
+    'name': "Customer Credit Limit",
+    'summary': """ Configure Credit Limit for Customers""",
+    'description': """ Activate and configure credit limit customer wise. If credit limit configured
+    the system will warn or block the confirmation of a sales order if the existing due amount is greater
+    than the configured warning or blocking credit limit. """,
+    'author': "Odoo Being",
+    'website': "https://www.odoobeing.com",
+    'license': 'AGPL-3',
+    'category': 'Sales',
+    'images': ['static/description/customer_credit_limit.png'],
+    'version': '15.0.1.0.0',
+    'support': 'odoobeing@gmail.com',
+    'depends': ['sale_management'],
     'data': [
-        'views/account_type_menu.xml'
+        'security/ir.model.access.csv',
+        'wizard/warning_wizard.xml',
+        'views/res_partner.xml',
+        'views/sale_order.xml',
     ],
-    'demo': [
-    ],
-    "images": ['static/description/Banner.png'],
-    'application': True,
     'installable': True,
     'auto_install': False,
-    'license': 'LGPL-3',
 }
